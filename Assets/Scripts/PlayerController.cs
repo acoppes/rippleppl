7384121a786886fcff97c4c3d5c171ea42fd122b
Ariel Coppes <ariel.coppes@gmail.com>
2017-01-20 22:45:46 -0300

    first code

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public string horizontalAxis = "Horizontal";
+	public string verticalAxis = "Vertical";
+
+	// Update is called once per frame
+	void Update () {
+
+		float dx = Input.GetAxis (horizontalAxis);
+		float dy = Input.GetAxis (verticalAxis);
+
+		transform.position = transform.position + new Vector3 (0, 1 * dy, 0);
+
+	}
+}
