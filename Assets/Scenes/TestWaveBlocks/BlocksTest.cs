db83ebfef4e4c4efa3e684117d0a9229e2f5a767
Ariel Coppes <ariel.coppes@gmail.com>
2017-01-21 16:00:26 -0300

    working on blocks animations

--- Assets/Scenes/TestWaveBlocks/BlocksTest.cs
+++ Assets/Scenes/TestWaveBlocks/BlocksTest.cs
@@ -1,16 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class BlocksTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	public TestWaveBlockAnimation[] blocks;
+	public float timeBetweenBlocks;
+
+	public float upTime;
+	public float downTime;
+
+	public float blockHeight;
+
 	// Update is called once per frame
 	void Update () {
-		
+		var power = GetPower ();
+
+		if (power == 0)
+			return;
+
+		StartCoroutine(BlocksAnimations (power));
+	}
+
+	IEnumerator BlocksAnimations(int power)
+	{
+		for (int i = 0; i < blocks.Length; i++) {
+			blocks [i].PlayAnimation (power, upTime, downTime, blockHeight);
+			yield return new WaitForSeconds (timeBetweenBlocks);
+		}
+
+		yield return null;
+	}
+
+	int GetPower()
+	{
+		KeyCode[] powerKeys = new KeyCode[] {
+			KeyCode.Alpha1,
+			KeyCode.Alpha2,
+			KeyCode.Alpha3,
+			KeyCode.Alpha4,
+			KeyCode.Alpha5
+		};
+
+		for (int i = 0; i < powerKeys.Length; i++) {
+			if (Input.GetKeyUp (powerKeys [i])) {
+				return i + 1;
+			}
+		}
+
+		return 0;
 	}
 }
--- Assets/Scenes/TestWaveBlocks/TestWaveBlockAnimation.cs
+++ Assets/Scenes/TestWaveBlocks/TestWaveBlockAnimation.cs
@@ -3,35 +3,48 @@ using System.Collections;
 
 public class TestWaveBlockAnimation : MonoBehaviour {
 
-	public float upTime;
-	public float downTime;
-
-	public float blockHeight;
-
 	public Transform model;
 
 	bool animating;
 
-	// Update is called once per frame
-	void Update () {
+//	// Update is called once per frame
+//	void Update () {
+//
+//		if (animating)
+//			return;
+//
+//		KeyCode[] powerKeys = new KeyCode[] {
+//			KeyCode.Alpha1,
+//			KeyCode.Alpha2,
+//			KeyCode.Alpha3,
+//			KeyCode.Alpha4,
+//			KeyCode.Alpha5
+//		};
+//
+//		for (int i = 0; i < powerKeys.Length; i++) {
+//			if (Input.GetKeyUp (powerKeys [i])) {
+//				StartCoroutine (PlayAnimation (i + 1));
+//			}
+//		}
+//
+//	}
 
+	float upTime;
+	float downTime;
+
+	float blockHeight;
+
+
+	public void PlayAnimation(int power, float upTime, float downTime, float blockHeight)
+	{
 		if (animating)
 			return;
 
-		KeyCode[] powerKeys = new KeyCode[] {
-			KeyCode.Alpha1,
-			KeyCode.Alpha2,
-			KeyCode.Alpha3,
-			KeyCode.Alpha4,
-			KeyCode.Alpha5
-		};
+		this.upTime = upTime;
+		this.downTime = downTime;
+		this.blockHeight = blockHeight;
 
-		for (int i = 0; i < powerKeys.Length; i++) {
-			if (Input.GetKeyUp (powerKeys [i])) {
-				StartCoroutine (PlayAnimation (i + 1));
-			}
-		}
-
+		StartCoroutine(PlayAnimationRoutine(power));
 	}
 
 	void IncrementPositionY(Transform t, float y)
@@ -39,7 +52,7 @@ public class TestWaveBlockAnimation : MonoBehaviour {
 		t.localPosition = t.localPosition + new Vector3 (0, y, 0);
 	}
 
-	IEnumerator PlayAnimation(int power)
+	IEnumerator PlayAnimationRoutine(int power)
 	{
 		animating = true;
 
