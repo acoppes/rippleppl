fcdc4c5ad06eb16941433fa61d2b84ae1a57b072
Ariel Coppes <ariel.coppes@gmail.com>
2017-01-22 10:47:50 -0300

    added character animations

---
+++ Assets/Scenes/TestCyberGirl/CybergirlAnimationsTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+//public class CharacterModel : MonoBehaviour
+//{
+//	
+//}
+
+public class CybergirlAnimationsTest : MonoBehaviour {
+
+	public Animator animator;
+
+	public void Update()
+	{
+		if (Input.GetKeyUp (KeyCode.Alpha1)) {
+			animator.Play ("Idle");
+		}
+		if (Input.GetKeyUp (KeyCode.Alpha2)) {
+			animator.Play ("Stomp");
+		}
+		if (Input.GetKeyUp (KeyCode.Alpha3)) {
+			animator.Play ("Stun");
+		}
+		if (Input.GetKeyUp (KeyCode.Alpha4)) {
+			animator.Play ("Charge");
+		}
+	}
+
+}
